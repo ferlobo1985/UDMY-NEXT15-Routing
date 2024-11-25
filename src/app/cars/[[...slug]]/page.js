24b86a01d89b54012872aac35a0acc58f324600e
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-25 02:16:15 -0300

    07-Catch all segments
    07-Catch all segments

---
+++ src/app/cars/[[...slug]]/page.js
@@ -0,0 +1,10 @@
+export default async function CarsPages({params}){
+    const slug = await params;
+    console.log(slug)
+
+    return(
+        <>
+            <h1>I am CARS</h1>
+        </>
+    )
+}

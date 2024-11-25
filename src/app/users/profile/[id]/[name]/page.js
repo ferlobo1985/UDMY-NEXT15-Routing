ee083f689e56d8f0fa8654c8a3c984c53bd68cd6
Coding rev <fernando.lobo.prez@gmail.com>
2024-11-25 02:04:39 -0300

    06-Params and Search params
    06-Params and Search params

--- src/app/users/profile/[id]/[name]/page.js
+++ src/app/users/profile/[id]/[name]/page.js
@@ -1,7 +1,13 @@
-export default function UserNamePage(){
+export default async function UserNamePage({params, searchParams}){
+    const id = (await params).id;
+    const name = (await params).name;
+
+    const query = await searchParams;
+    console.log(query)
+
     return(
         <>
-            <h1>I am USER NAME PAGE</h1>
+            <h1>{id} is the ID, {name} is the name</h1>
         </>
     )
 }
